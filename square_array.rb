19f049b8a0e6e26761c1c983a39fdea3d8ec19cc
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-07-08 16:17:50 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,13 @@
 def square_array(array)
-  # your code here
+  new_array = []
+  array.each{|a| new_array.push(a*a)}
+  return new_array
 end
+
+arr = [1, 2, 3, 4, 5]
+square_array(arr)
+# //arr.each { |a| print a -= 10, " " }
+# # prints: -9 -8 -7 -6 -5
+#=> [1, 2, 3, 4, 5]
+
+# basket.each do |apple|
