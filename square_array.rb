271ed6a0b52e50ce6965af6db38344ea419c907d
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-07-08 16:08:53 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,12 +1,2 @@
 
-def square_array(numbers)
-    numbers.each do |num|
-     new_numbers << (num ** 2)
-  end
-  return new_numbers
-end
 
-def square_array(numbers)
-  new_numbers = numbers.collect{|num| num ** 2}
-  return new_numbers 
-end
